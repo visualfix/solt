e5adc41e45572af30b8ce3a3ab7ede86616a1497
Im HyungJin <vf0918@gmail.com>
2021-11-28 15:48:59 +0900

    add test client

---
+++ src/testClient/main.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+#include <array>
+#include <asio.hpp>
+
+using asio::ip::tcp;
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 3)
+    {
+      std::cerr << "Usage: client <host>" << std::endl;
+      return 1;
+    }
+
+    asio::io_context io_context;
+
+    tcp::resolver resolver(io_context);
+    tcp::resolver::results_type endpoints =
+      resolver.resolve(argv[1], argv[2], tcp::resolver::query::numeric_service);
+
+    tcp::socket socket(io_context);
+    asio::connect(socket, endpoints);
+
+    for (;;)
+    {
+      std::array<char, 128> buf;
+      asio::error_code error;
+
+      size_t len = socket.read_some(asio::buffer(buf), error);
+
+      if (error == asio::error::eof)
+        break; // Connection closed cleanly by peer.
+      else if (error)
+        throw asio::system_error(error); // Some other error.
+
+      std::cout.write(buf.data(), len);
+    }
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return 0;
+}
